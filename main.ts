63cae986ae5e625fa8cd45417f627fb40dd140a4
Michael Wagner <maffelbaffel@posteo.de>
2022-12-21 19:48:17 +0100

    feat: implemented initial version of XournalppPlugin
    Added xournalpp code block processor that allows to input a path to a xopp in the vault. That

--- main.ts
+++ main.ts
@@ -1,81 +1,84 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {Plugin, TFile, TFolder} from 'obsidian';
+import {spawn} from "child_process";
+import * as path from "path";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
+interface XournalppPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: XournalppPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class XournalppPlugin extends Plugin {
+	settings: XournalppPluginSettings;
 
 	async onload() {
+		console.log("Loading XournalppPlugin")
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerMarkdownCodeBlockProcessor('xournalpp', async (source, el, ctx) => {
+			const sourceLines = source.trim().split('\n');
+			const url = sourceLines[0];
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			let sourceFile = this.app.vault.getAbstractFileByPath(url)
+			if (sourceFile == null) {
+				this.showError(el, `Source '${url}' is not found`)
+			} else if (sourceFile instanceof TFile) {
+				await this.generatePNG(sourceFile)
+					.then(generatedUrl => {
+						this.createThumbnail(el, generatedUrl)
+					}).catch(err => {
+						this.showError(el, `Convert failed with error code  ${err}`)
+					})
+			} else if (sourceFile instanceof TFolder) {
+				this.showError(el, `Source '${sourceFile.path}' is a folder. Should be .xopp`)
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+	}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+	async generatePNG(file: TFile): Promise<TFile> {
+		let targetPath = path.format({...path.parse(file.path), base: '', ext: '.png'})
+		console.log(`Exporting ${file.path} to ${targetPath}`);
+
+		let prc = spawn('xournalpp', ['-i', targetPath, file.path], {cwd: this.app.vault.adapter['basePath']}); // , '--export-png-height=500'
+		prc.stdout.setEncoding('utf8');
+		prc.stdout.on('data', function (data) {
+			let str = data.toString()
+			let lines = str.split(/(\r?\n)/g);
+			console.log(lines.join(""));
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		return new Promise((resolve, reject) => {
+			prc.on('close', async (code) => {
+				console.log('process exit code ' + code);
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+				if (code !== 0)
+					reject(code);
+
+				let result = this.app.vault.getAbstractFileByPath(targetPath)
+				resolve(result);
+			});
 		});
+	}
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	createThumbnail(el: HTMLElement, file: TFile) {
+		let thumbnailUrl = this.app.vault.getResourcePath(file);
+
+		const container = el.createEl('a', {
+			href: `app://obsidian.md/${file.path}`,
+			target: '_blank',
+			rel: "noopener"
+		});
+		container.addClass('thumbnail');
+		container.createEl('img', {attr: {'src': thumbnailUrl}}).addClass('thumbnail-img');
+		const textBox = container.createDiv();
+		textBox.addClass('thumbnail-text');
+	}
+
+	showError(el: HTMLElement, error: string) {
+		const span = el.createEl('span');
+		span.innerText = error;
 	}
 
 	onunload() {
@@ -88,50 +91,5 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
