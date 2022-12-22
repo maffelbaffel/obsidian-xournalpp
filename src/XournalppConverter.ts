834c455e7265a8c698129cb2a1131a8e364e59c7
Michael Wagner <maffelbaffel@posteo.de>
2022-12-22 11:49:51 +0100

    chore: moved export logic to separate XournalppConverter

---
+++ src/XournalppConverter.ts
@@ -0,0 +1,32 @@
+import {App, debounce, Debouncer, TFile} from "obsidian";
+import {Source} from "./Source";
+import {spawn} from "child_process";
+import {IGNORE_HEIGHT, IGNORE_WIDTH} from "./main";
+import * as path from "path";
+
+export class XournalppConverter {
+	constructor(private readonly app: App) {
+	}
+
+	async generatePNG(file: TFile, source: Source): Promise<TFile> {
+		let targetPath = path.format({...path.parse(file.path), base: '', ext: '.png'})
+		let target = this.app.vault.getAbstractFileByPath(targetPath) as TFile
+		console.debug(`Exporting ${file.path} to ${targetPath}`);
+
+		let args = ['-i', targetPath, file.path, `--export-range=${source.pages}`];
+		if (source.height !== IGNORE_HEIGHT)
+			args.push(`--export-png-height=${source.height}`)
+		if (source.width !== IGNORE_WIDTH)
+			args.push(`--export-png-width=${source.width}`)
+
+		let prc = spawn('xournalpp', args, {cwd: (<any>this.app.vault.adapter)['basePath']});
+		return new Promise((resolve, reject) => {
+			prc.on('close', async (code) => {
+				if (code !== 0)
+					reject(code);
+				else
+					resolve(target);
+			});
+		});
+	}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-import {Plugin, TFile, TFolder} from 'obsidian';
-import {spawn} from "child_process";
-import * as path from "path";
+import {debounce, Plugin, TFile, TFolder} from 'obsidian';
 import {Source} from "./Source";
 import {XournalppSettingTab} from "./ui/XournalppSettingTab";
 import {XournalppPluginSettings} from "./XournalppPluginSettings";
 import {Thumbnail} from "./ui/Thumbnail";
+import {XournalppConverter} from "./XournalppConverter";
 
-const IGNORE_HEIGHT = 0;
-const IGNORE_WIDTH = 0;
+export const IGNORE_HEIGHT = 0;
+export const IGNORE_WIDTH = 0;
 const DEFAULT_SETTINGS: XournalppPluginSettings = {
 	defaultHeight: IGNORE_HEIGHT,
 	defaultWidth: IGNORE_WIDTH
@@ -15,58 +14,36 @@ const DEFAULT_SETTINGS: XournalppPluginSettings = {
 
 export default class XournalppPlugin extends Plugin {
 	settings: XournalppPluginSettings;
+	private converter = new XournalppConverter(this.app)
 
 	async onload() {
-		console.log("Loading XournalppPlugin")
+		console.debug("Loading XournalppPlugin")
+		let processDebouncer = debounce(async (sourceText: string, el: HTMLElement) => this.process(sourceText, el), 50);
+
 		await this.loadSettings();
-
 		this.addSettingTab(new XournalppSettingTab(this.app, this));
-		this.registerMarkdownCodeBlockProcessor('xournalpp', async (sourceText, el, ctx) => {
-			const source = Source.parseInput(sourceText, this.settings)
+		this.registerMarkdownCodeBlockProcessor('xournalpp', async (sourceText, el) => processDebouncer(sourceText, el));
+	}
 
-			let sourceFile = this.app.vault.getAbstractFileByPath(source.file)
-			if (sourceFile == null) {
-				this.showError(el, `Source '${source.file}' is not found`)
-			} else if (sourceFile instanceof TFile) {
-				await this.generatePNG(sourceFile, source)
+	async process(sourceText: string, el: HTMLElement) {
+		const source = Source.parseInput(sourceText, this.settings)
+
+		let sourceFile = this.app.vault.getAbstractFileByPath(source.file)
+		if (sourceFile == null) {
+			this.showError(el, `Source '${source.file}' is not found`)
+		} else if (sourceFile instanceof TFile) {
+			if (sourceFile.extension !== 'xopp')
+				this.showError(el, `Source '${sourceFile.path}' is not a .xopp`)
+			else
+				await this.converter.generatePNG(sourceFile, source)
 					.then(generatedUrl => {
 						this.createThumbnail(el, generatedUrl)
 					}).catch(err => {
 						this.showError(el, `Convert failed with error code  ${err}`)
 					})
-			} else if (sourceFile instanceof TFolder) {
-				this.showError(el, `Source '${sourceFile.path}' is a folder. Should be .xopp`)
-			}
-		});
-	}
-
-
-	async generatePNG(file: TFile, source: Source): Promise<TFile> {
-		let targetPath = path.format({...path.parse(file.path), base: '', ext: '.png'})
-		console.log(`Exporting ${file.path} to ${targetPath}`);
-
-		let args = ['-i', targetPath, file.path, `--export-range=${source.pages}`];
-		if (source.height !== IGNORE_HEIGHT)
-			args.push(`--export-png-height=${source.height}`)
-		if (source.width !== IGNORE_WIDTH)
-			args.push(`--export-png-width=${source.width}`)
-
-		let prc = spawn('xournalpp', args, {cwd: (<any>this.app.vault.adapter)['basePath']});
-		prc.stdout.setEncoding('utf8');
-		prc.stdout.on('data', function (data) {
-			let str = data.toString()
-			let lines = str.split(/(\r?\n)/g);
-			console.log(lines.join(""));
-		});
-
-		return new Promise((resolve, reject) => {
-			prc.on('close', async (code) => {
-				if (code !== 0)
-					reject(code);
-				else
-					resolve(this.app.vault.getAbstractFileByPath(targetPath) as TFile);
-			});
-		});
+		} else if (sourceFile instanceof TFolder) {
+			this.showError(el, `Source '${sourceFile.path}' is a folder. Should be .xopp`)
+		}
 	}
 
 	createThumbnail(el: HTMLElement, file: TFile) {
